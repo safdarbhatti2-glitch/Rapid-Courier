51f9bf1b5b3cdb41beb1f7aa3e5b6758fc0e8043
Safdar Bhatti <safdarbhatti2@gmail.com>
2026-09-05 21:26:30 +0500

    Remove deploy hook

--- public/update_rc.php
+++
@@ -1,5 +0,0 @@
-<?php
-$token = $_GET['key'] ?? '';
-if ($token !== 'rc_deploy_2026') { die('Access Denied'); }
-$base = dirname(__DIR__);
-echo shell_exec("cd " . escapeshellarg($base) . " && git fetch origin main 2>&1 && git reset --hard origin/main 2>&1");
